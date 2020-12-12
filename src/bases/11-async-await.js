ede5521ef567e30ce81231e4f618c0cc5af9a9b7
Luis Ponce <luisponce@Luiss-MacBook-Pro.local>
2020-12-12 00:17:13 -0600

    He agregado el contenido del ternario

--- src/bases/11-async-await.js
+++ src/bases/11-async-await.js
@@ -1,1 +1,16 @@
+const activo = true;
 
+// let mensaje = '';
+
+// if (activo) {
+//   mensaje = "Activo";
+// } else {
+//   mensaje = "Inactivo";
+// }
+
+// const mensaje = activo ? "Activo" : "Inactivo";
+// const mensaje = activo ? "Activo" : null;
+
+const mensaje = activo === true && "Activo";
+
+console.log(mensaje);
